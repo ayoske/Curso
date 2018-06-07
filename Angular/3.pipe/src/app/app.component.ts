252cfc586340f8f58fb37f4ee0a58dbc66a8d91e
Armando <ayoske_fullforce@hotmail.com>
2018-06-07 15:20:01 -0500

    Videos con ejercicios hechos del 73 al 79

--- Angular/3.pipe/src/app/app.component.ts
+++ Angular/3.pipe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resolve, reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,28 @@ export class AppComponent {
   nombre = 'Armando';
 
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = .234;
+
+  salario = 243.5;
+
+  hombre = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 'Mansion x'
+    }
+  };
+
+  // tslint:disable-next-line:no-shadowed-variable
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(() => resolve('Llegó la data'), 3500);
+  });
+
+  fecha = new Date();
 
 }
--- Angular/3.pipe/src/app/app.module.ts
+++ Angular/3.pipe/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -10,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
