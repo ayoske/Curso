5ee4d6935d8c9a418f74555969b2696efa1d3453
Armando <ayoske_fullforce@hotmail.com>
2018-06-15 16:06:17 -0500

    video 93 terminado conbsumiendo servicio

--- Angular/4.spotiapp/src/app/services/spotify.service.ts
+++ Angular/4.spotiapp/src/app/services/spotify.service.ts
@@ -11,11 +11,9 @@ export class SpotifyService {
   getNewRelease() {
 
     const headers: HttpHeaders = new HttpHeaders({
-       'Authorization': 'Bearer BQBr6AG_Ls5X8qFtWJsgdoEE0tWAT92sAls0G16Qbp6MJq25MEd8ui2l8GedUF3nMsNZ9QKFb7_DJFOVal4'
+       'Authorization': 'Bearer BQCl4vkvRFbaJ-lWE_y-kWFti7sTlAEguvLb3ACuWVJZC3ay2HiiIL-hiYxSQtYUKVAeh4vvMSKpl2Dj32w'
     });
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers }).subscribe( data => {
-      console.log( data );
-    });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
 }
