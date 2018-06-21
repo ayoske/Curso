62ebe4959a5a1e3018b450e517fea17580883186
Armando <ayoske_fullforce@hotmail.com>
2018-06-21 14:12:43 -0500

    Video 100 Página del artista, nueva ruta, parámetro por url y servicio

--- Angular/4.spotiapp/src/app/components/artist/artist.component.ts
+++ Angular/4.spotiapp/src/app/components/artist/artist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-artist',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRouter: ActivatedRoute ) {
+    this.activatedRouter.params.subscribe( params => {
+       console.log( params );
+
+    });
+  }
 
   ngOnInit() {
   }
--- Angular/4.spotiapp/src/app/components/tarjeta/tarjeta.component.ts
+++ Angular/4.spotiapp/src/app/components/tarjeta/tarjeta.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tarjeta',
@@ -10,6 +11,23 @@ export class TarjetaComponent {
   // Se importa variable items, que viene de donde se usa el componente tarjeta "<app-tarjeta [items]="artistas" >"
   @Input() items: any[] = [];
 
-  constructor() { }
+  constructor( private router: Router ) { }
+
+  verArtista( item: any ) {
+
+    let artistaId;
+
+    if (item.type === 'artist') {
+      artistaId = item.id;
+    } else {
+      artistaId = item.artists[0].id;
+    }
+
+    console.log(artistaId);
+
+    this.router.navigate(['/artist', artistaId]);
+
+
+  }
 
 }
