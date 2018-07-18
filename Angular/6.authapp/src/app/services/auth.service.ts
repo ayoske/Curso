1c2aa91720a586d64a63180b3a26bdacf4152853
Robin_job <ayoske_fullforce@hotmail.com>
2018-07-18 11:12:27 -0500

    Video 148 y 149 navbar, rutas, auth0
    creación de navbar, de rutas y boton login, se creó el servicio auth.service

---
+++ Angular/6.authapp/src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+// src/app/auth/auth.service.ts
+
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import * as auth0 from 'auth0-js';
+
+(window as any).global = window;
+
+@Injectable()
+export class AuthService {
+
+  auth0 = new auth0.WebAuth({
+    clientID: 'VqcHyANrTP55dpfBqm5HepElPJs0VUSm',
+    domain: 'robinsdevelop.auth0.com',
+    responseType: 'token id_token',
+    audience: 'https://robinsdevelop.auth0.com/userinfo',
+    redirectUri: 'http://localhost:3000/callback',
+    scope: 'openid'
+  });
+
+  constructor(public router: Router) {}
+
+  public login(): void {
+    this.auth0.authorize();
+  }
+}
